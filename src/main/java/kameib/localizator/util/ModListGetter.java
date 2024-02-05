70e31c234563de60274c71bc5af81a9c3f196d9f
KameiB <kameib.twitch@gmail.com>
2024-02-05 05:18:13 -0600

    Transition to FermiumBooter

---
+++ src/main/java/kameib/localizator/util/ModListGetter.java
@@ -0,0 +1,81 @@
+package kameib.localizator.util;
+
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import kameib.localizator.Localizator;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.jar.JarEntry;
+import java.util.jar.JarFile;
+
+public class ModListGetter {
+    
+    public static List<String> getModList() {
+        List<String> modList = new ArrayList<>();
+        File modsFolder = new File("mods");
+
+        if (modsFolder.exists() && modsFolder.isDirectory()) {
+            File[] modFiles = modsFolder.listFiles();
+
+            if (modFiles != null) {
+                System.out.println("Installed Mods:");
+
+                String modId;
+                for (File modFile : modFiles) {
+                    if (modFile.isFile() && modFile.getName().endsWith(".jar")) {
+                        if (modFile.getName().contains(Localizator.NAME)) {
+                            continue;
+                        }
+                        modId = getModIdFromJar(modFile);
+                        modList.add(modId);
+                        System.out.println("Mod Name: " + modFile.getName() + ", Mod ID: " + modId);
+                    }
+                }
+            } else {
+                System.out.println("No mods found in the mods folder.");
+            }
+        } else {
+            System.out.println("Mods folder does not exist or is not a directory.");
+        }
+        return modList;
+    }
+
+    private static String getModIdFromJar(File modFile) {
+        try (JarFile jarFile = new JarFile(modFile)) {
+            JarEntry entry = jarFile.getJarEntry("mcmod.info");
+            if (entry != null) {
+                // Use a library like Gson to parse JSON
+                JsonParser parser = new JsonParser();
+                JsonElement element = parser.parse(readInputStream(jarFile.getInputStream(entry)));
+
+                if (element.isJsonArray()) {
+                    JsonArray modsArray = element.getAsJsonArray();
+                    if (modsArray.size() > 0) {
+                        JsonObject modInfo = modsArray.get(0).getAsJsonObject();
+                        if (modInfo.has("modid")) {
+                            return modInfo.get("modid").getAsString();
+                        }
+                    }
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return "Unknown";
+    }
+
+    private static String readInputStream(InputStream inputStream) throws IOException {
+        StringBuilder resultStringBuilder = new StringBuilder();
+        byte[] buffer = new byte[1024];
+        int length;
+        while ((length = inputStream.read(buffer)) != -1) {
+            resultStringBuilder.append(new String(buffer, 0, length));
+        }
+        return resultStringBuilder.toString();
+    }
+}
