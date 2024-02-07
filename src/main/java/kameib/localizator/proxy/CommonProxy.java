694d1bbabbf0a4b54721eee70f75830c9a97c52c
KameiB <kameib.twitch@gmail.com>
2024-02-07 14:30:28 -0600

    seems like idk how to revert commits lol

--- src/main/java/kameib/localizator/proxy/CommonProxy.java
+++ src/main/java/kameib/localizator/proxy/CommonProxy.java
@@ -2,14 +2,13 @@ package kameib.localizator.proxy;
 
 
 import kameib.localizator.client.event.NewConfigEvent;
-import kameib.localizator.common.networking.PrimaryPacketHandler;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 
 public class CommonProxy {
 
     public void preInit() {
-        PrimaryPacketHandler.registerPackets();
+        
     }
     
     public void onInit(FMLInitializationEvent event) {
