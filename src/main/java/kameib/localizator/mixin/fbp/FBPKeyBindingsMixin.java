08a56183ef5a3562346a21c183f98e01299ab686
KameiB <kameib.twitch@gmail.com>
2025-02-19 20:52:49 -0600

    🐛 [FBP] (Finally?) Fixed the Keybind descriptions + R should work now.

--- src/main/java/kameib/localizator/mixin/fbp/FBPKeyBindingsMixin.java
+++ src/main/java/kameib/localizator/mixin/fbp/FBPKeyBindingsMixin.java
@@ -1,47 +1,39 @@
 package kameib.localizator.mixin.fbp;
 
 import com.TominoCZ.FBP.keys.FBPKeyBindings;
-import net.minecraft.client.settings.KeyBinding;
+import kameib.localizator.data.Production;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
-import org.lwjgl.input.Keyboard;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.*;
 
 @SideOnly(Side.CLIENT)
 @Mixin(FBPKeyBindings.class)
 public abstract class FBPKeyBindingsMixin {
-    @Inject(
+    @ModifyArg(
             method = "init()V",
             at = @At(
                     value = "INVOKE",
-                    target = "Lnet/minecraftforge/fml/client/registry/ClientRegistry;registerKeyBinding(Lnet/minecraft/client/settings/KeyBinding;)V",
-                    ordinal = 0,
-                    remap = false
+                    target = "Lnet/minecraft/client/settings/KeyBinding;<init>(Ljava/lang/String;ILjava/lang/String;)V",
+                    remap = Production.inProduction
             ),
+            index = 0,
             remap = false
     )
-    // IDK why FBPMenu is still hardcoded...
-    private static void Localizator_FBP_FBPKeyBindings_init_beforeRegisterKeyBindings(CallbackInfo ci) {
-        
-        FBPFreeze = new KeyBinding("key.fbp.toggle_freeze", Keyboard.KEY_R, "Fancy Block Particles");
-        FBPToggle = new KeyBinding("key.fbp.enable_disable", Keyboard.KEY_NONE, "Fancy Block Particles");
-        FBPSweep = new KeyBinding("key.fbp.kill_particles", Keyboard.KEY_NONE, "Fancy Block Particles");
-        FBPFastAdd = new KeyBinding("key.fbp.blacklist_block", Keyboard.KEY_X, "Fancy Block Particles");
-        FBPMenu = new KeyBinding("key.fbp.open_menu", Keyboard.KEY_P, "Fancy Block Particles");
+    // This should correctly localize the descriptions and keep the R keybind working
+    private static String Localizator_FBP_FBPKeyBindings_init_mapKeyDescriptions(String description) {
+        switch (description) {
+            case "Open Menu":
+                return "key.fbp.open_menu";
+            case "Toggle Freeze Effect":
+                return "key.fbp.toggle_freeze";
+            case "Enable/Disable":
+                return "key.fbp.enable_disable";
+            case "Kill Particles":
+                return "key.fbp.kill_particles";
+            case "Blacklist Block":
+                return "key.fbp.blacklist_block";
+        }
+        return description;
     }
-    
-    @Shadow(remap = false)
-    public static KeyBinding FBPMenu;
-    @Shadow(remap = false)
-    public static KeyBinding FBPFreeze;
-    @Shadow(remap = false)
-    public static KeyBinding FBPToggle;
-    @Shadow(remap = false)
-    public static KeyBinding FBPSweep;
-    @Shadow(remap = false)
-    public static KeyBinding FBPFastAdd;
 }
