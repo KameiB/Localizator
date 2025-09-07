39b616c5b94b2c81c15f4de5dd1344d2ea65c1fb
Nischhelm <nischhelm@gmail.com>
2025-09-07 21:22:17 +0200

    SRP missing lang translation + mixin
    missing es_mx translation

---
+++ src/main/java/kameib/localizator/mixin/srparasites/EntityParasiticScentMixin.java
@@ -0,0 +1,44 @@
+package kameib.localizator.mixin.srparasites;
+
+import com.dhanantry.scapeandrunparasites.entity.EntityParasiticScent;
+import net.minecraft.util.text.ITextComponent;
+import net.minecraft.util.text.TextComponentTranslation;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.ModifyArg;
+
+@Mixin(EntityParasiticScent.class)
+public abstract class EntityParasiticScentMixin {
+    @ModifyArg(
+            method = "scentListener",
+            at = @At(
+                    value = "INVOKE",
+                    target = "Lcom/dhanantry/scapeandrunparasites/entity/EntityParasiticScent;warnPlayers(Ljava/lang/String;)V",
+                    ordinal = 0,
+                    remap = false
+            ),
+            remap = false
+    )
+    // Replace the hardcoded message with a lang key
+    // Line 157: this.warnPlayers("Scent is active");
+    private static String SRParasites_EntityParasiticScent_scentListener_warnPlayers(String message) {
+        return "message.srparasites.scentactive";
+    }
+
+    @ModifyArg(
+            method = "warnPlayers",
+            at = @At(
+                    value = "INVOKE",
+                    target = "Lnet/minecraft/entity/player/EntityPlayer;func_146105_b(Lnet/minecraft/util/text/ITextComponent;Z)V",
+                    ordinal = 0,
+                    remap = false
+            ),
+            index = 0,
+            remap = false
+    )
+    // Replace the hardcoded text with a lang key
+    // Line 647: mob.func_146105_b(new TextComponentString(in), true);
+    private static ITextComponent SRParasite_EntityParasiticScent_warnPlayers_sendStatusMessage(ITextComponent message) {
+        return new TextComponentTranslation(message.getUnformattedComponentText());
+    }
+}
