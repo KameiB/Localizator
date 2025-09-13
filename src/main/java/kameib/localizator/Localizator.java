5ffd8cf7afd34cd2f4d260e07cb1e443b2caba83
kameib <kameib.twitch@gmail.com>
2025-09-14 03:15:18 +0900

    🚀 Bump to version 1.0.14

--- src/main/java/kameib/localizator/Localizator.java
+++ src/main/java/kameib/localizator/Localizator.java
@@ -13,7 +13,7 @@ import org.apache.logging.log4j.Logger;
 @Mod(modid = Localizator.MODID, version = Localizator.VERSION, name = Localizator.NAME)
 public class Localizator {
     public static final String MODID = "localizator";
-    public static final String VERSION = "1.0.13";
+    public static final String VERSION = "1.0.14";
     public static final String NAME = "Localizator";
     public static final Logger LOGGER = LogManager.getLogger();
 
