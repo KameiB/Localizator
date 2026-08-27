e9ad57bde22e9834f8e0fb6c4996f994c97ca5fe
KameiB <111140918+KameiB@users.noreply.github.com>
2026-08-27 23:00:59 +0900

    - Bisect Hosting - Improved the injections to ModifyConstant so they become safer.

--- src/main/java/kameib/localizator/mixin/bhmenu/RecommendJarStepMixin.java
+++ src/main/java/kameib/localizator/mixin/bhmenu/RecommendJarStepMixin.java
@@ -5,50 +5,32 @@ import net.minecraft.client.resources.I18n;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.ModifyArg;
+import org.spongepowered.asm.mixin.injection.Constant;
+import org.spongepowered.asm.mixin.injection.ModifyConstant;
 
 @Mixin(RecommendJarStep.class)
 public abstract class RecommendJarStepMixin {
     // Replace the hardcoded "Select" text with its intended lang key
     // Line 31: new PressableButton(1, screen.width / 2 - 104, screen.height / 2 + 20, 100, 20, "Select", (b)
     @SideOnly(Side.CLIENT)
-    @ModifyArg(
+    @ModifyConstant(
             method = "init(Lcom/bisecthosting/mods/bhmenu/modules/servercreatorbanner/screens/BHOrderScreen;Ljava/util/function/Consumer;)V",
-            at = @At(
-                    value = "INVOKE",
-                    target = "com/bisecthosting/mods/bhmenu/client/screen/PressableButton.<init> (IIIIILjava/lang/String;Ljava/util/function/Consumer;)V",
-                    ordinal = 0,
-                    remap = false
-            ),
-            index = 5,
+            constant = @Constant(stringValue = "Select"),
             remap = false
     )
     private String BHMenu_RecommendJarStep_init_newPressableButton_Select(String buttonText) {
-        if (buttonText.equals("Select")) {
-            return I18n.format("step.recommended_jar.select");
-        }
-        return buttonText;
+        return I18n.format("step.recommended_jar.select");
     }
 
     // Replace the hardcoded "Change" text with its intended lang key
     // Line 35: new PressableButton(2, screen.width / 2 + 4, screen.height / 2 + 20, 100, 20, "Change", (b) -> screen.setStep(new SelectJarStep(this.orderData))));
     @SideOnly(Side.CLIENT)
-    @ModifyArg(
+    @ModifyConstant(
             method = "init(Lcom/bisecthosting/mods/bhmenu/modules/servercreatorbanner/screens/BHOrderScreen;Ljava/util/function/Consumer;)V",
-            at = @At(
-                    value = "INVOKE",
-                    target = "com/bisecthosting/mods/bhmenu/client/screen/PressableButton.<init> (IIIIILjava/lang/String;Ljava/util/function/Consumer;)V",
-                    ordinal = 1,
-                    remap = false
-            ),
-            index = 5,
+            constant = @Constant(stringValue = "Change"),
             remap = false
     )
     private String BHMenu_RecommendJarStep_init_newPressableButton_Change(String buttonText) {
-        if (buttonText.equals("Change")) {
-            return I18n.format("step.recommended_jar.change");
-        }
-        return buttonText;
+        return I18n.format("step.recommended_jar.change");
     }
 }
