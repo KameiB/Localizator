194ab45089d9f6258f9701ac2bb8200e17ea0818
KameiB <111140918+KameiB@users.noreply.github.com>
2026-09-02 21:22:56 +0900

    - BisectHosting Menu - Forgot to add a comment and rename the method to the new standard starting with "localizator_", because Mixin-using mods are getting popular.

--- src/main/java/kameib/localizator/mixin/bhmenu/RecommendJarStepMixin.java
+++ src/main/java/kameib/localizator/mixin/bhmenu/RecommendJarStepMixin.java
@@ -10,27 +10,27 @@ import org.spongepowered.asm.mixin.injection.ModifyConstant;
 
 @Mixin(RecommendJarStep.class)
 public abstract class RecommendJarStepMixin {
-    // Replace the hardcoded "Select" text with its intended lang key
-    // Line 31: new PressableButton(1, screen.width / 2 - 104, screen.height / 2 + 20, 100, 20, "Select", (b)
     @SideOnly(Side.CLIENT)
     @ModifyConstant(
             method = "init(Lcom/bisecthosting/mods/bhmenu/modules/servercreatorbanner/screens/BHOrderScreen;Ljava/util/function/Consumer;)V",
             constant = @Constant(stringValue = "Select"),
             remap = false
     )
-    private String BHMenu_RecommendJarStep_init_newPressableButton_Select(String buttonText) {
+    // Replace the hardcoded "Select" text with its intended lang key
+    // Line 31: new PressableButton(1, screen.width / 2 - 104, screen.height / 2 + 20, 100, 20, "Select", (b)
+    private String localizator_BHMenu_RecommendJarStep_init_newPressableButton_Select(String buttonText) {
         return I18n.format("step.recommended_jar.select");
     }
 
-    // Replace the hardcoded "Change" text with its intended lang key
-    // Line 35: new PressableButton(2, screen.width / 2 + 4, screen.height / 2 + 20, 100, 20, "Change", (b) -> screen.setStep(new SelectJarStep(this.orderData))));
     @SideOnly(Side.CLIENT)
     @ModifyConstant(
             method = "init(Lcom/bisecthosting/mods/bhmenu/modules/servercreatorbanner/screens/BHOrderScreen;Ljava/util/function/Consumer;)V",
             constant = @Constant(stringValue = "Change"),
             remap = false
     )
-    private String BHMenu_RecommendJarStep_init_newPressableButton_Change(String buttonText) {
+    // Replace the hardcoded "Change" text with its intended lang key
+    // Line 35: new PressableButton(2, screen.width / 2 + 4, screen.height / 2 + 20, 100, 20, "Change", (b) -> screen.setStep(new SelectJarStep(this.orderData))));
+    private String localizator_BHMenu_RecommendJarStep_init_newPressableButton_Change(String buttonText) {
         return I18n.format("step.recommended_jar.change");
     }
 }
