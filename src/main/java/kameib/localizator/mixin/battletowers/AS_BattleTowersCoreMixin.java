bc4dcbe766708bc4c98c6e3deab1c5b84ac466ac
KameiB <111140918+KameiB@users.noreply.github.com>
2026-09-02 21:18:11 +0900

    - BatteTowers - Improved granularity on AS_BattleTowersCoreMixin. Now we only replace the argument that passes to the TextComponentTranslation instantiation, instead of creating a new SPacketChat, then instantiating a new TextComponentTranslation, and finally using our lang key.

--- src/main/java/kameib/localizator/mixin/battletowers/AS_BattleTowersCoreMixin.java
+++ src/main/java/kameib/localizator/mixin/battletowers/AS_BattleTowersCoreMixin.java
@@ -2,9 +2,6 @@ package kameib.localizator.mixin.battletowers;
 
 import atomicstryker.battletowers.common.AS_BattleTowersCore;
 import kameib.localizator.data.Production;
-import net.minecraft.network.Packet;
-import net.minecraft.network.play.server.SPacketChat;
-import net.minecraft.util.text.TextComponentTranslation;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.ModifyArg;
@@ -15,14 +12,15 @@ public abstract class AS_BattleTowersCoreMixin {
             method = "onBattleTowerDestroyed(Latomicstryker/battletowers/common/AS_TowerDestroyer;)V",
             at = @At(
                     value = "INVOKE",
-                    target = "Lnet/minecraft/server/management/PlayerList;sendPacketToAllPlayers(Lnet/minecraft/network/Packet;)V",
+                    target = "Lnet/minecraft/util/text/TextComponentTranslation;<init>(Ljava/lang/String;[Ljava/lang/Object;)V",
+                    ordinal = 0,
                     remap = Production.inProduction
             ),
             remap = false
     )
-    // Send a lang key instead of a hardcoded text
+    // Send a lang key instead of a hardcoded text as an argument to the already created TextComponentTranslation object
     // Line 174: FMLCommonHandler.instance().getMinecraftServerInstance().getPlayerList().sendPacketToAllPlayers(new SPacketChat(new TextComponentTranslation("A Battletower's Guardian has fallen! Without it's power, the Tower will collapse...", new Object[0])));
-    private static net.minecraft.network.Packet<?> BattleTowers_AS_BattleTowers_onBattleTowerDestroyed_sendTextComponentTranslation(Packet<?> packetIn) {
-        return new SPacketChat(new TextComponentTranslation("notif.battletowers.golem_defeated"));
+    private static String localizator_BattleTowers_AS_BattleTowers_onBattleTowerDestroyed_sendTextComponentTranslation(String message) {
+        return "notif.battletowers.golem_defeated";
     }
 }
