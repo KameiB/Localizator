f9dcdba560c41768de07b6b677909842aed52466
KameiB <111140918+KameiB@users.noreply.github.com>
2026-09-02 21:46:18 +0900

    - DynamicTrees - Replaced addInformation()'s disguised Overwrite -> ModifyConstant injections, so now it should be compatible with DramaticTrees?

--- src/main/java/kameib/localizator/mixin/dynamictrees/StaffMixin.java
+++ src/main/java/kameib/localizator/mixin/dynamictrees/StaffMixin.java
@@ -1,47 +1,33 @@
 package kameib.localizator.mixin.dynamictrees;
 
 import com.ferreusveritas.dynamictrees.items.Staff;
-import com.ferreusveritas.dynamictrees.trees.Species;
 import kameib.localizator.data.Production;
-import net.minecraft.client.resources.I18n;
-import net.minecraft.client.util.ITooltipFlag;
-import net.minecraft.item.ItemStack;
 import net.minecraft.util.text.TextFormatting;
-import net.minecraft.world.World;
-import net.minecraftforge.fml.relauncher.Side;
-import net.minecraftforge.fml.relauncher.SideOnly;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-
-import java.util.List;
+import org.spongepowered.asm.mixin.injection.Constant;
+import org.spongepowered.asm.mixin.injection.ModifyConstant;
 
 @Mixin(Staff.class)
-public abstract class StaffMixin {    
-    /**
-     * @author KameiB
-     * @reason I just HAD to remove that weird "Â" symbol from the tooltip... 
-     */
-    @Inject(
+public abstract class StaffMixin {
+    @ModifyConstant(
             method = "addInformation(Lnet/minecraft/item/ItemStack;Lnet/minecraft/world/World;Ljava/util/List;Lnet/minecraft/client/util/ITooltipFlag;)V",
-            at = @At("HEAD"),
-            cancellable = true,
+            constant = @Constant(stringValue = "\u00c2\u00a7a"),
             remap = Production.inProduction
     )
-    @SideOnly(Side.CLIENT)
-    // Line 272
-    private void DynamicTrees_Staff_addInformation(ItemStack stack, World world, List<String> tooltip, ITooltipFlag flagIn, CallbackInfo ci) {
-        Species species = this.getSpecies(stack);
-        tooltip.add(I18n.format("tooltip.woodland_staff.tree") + " " + (species != null ? TextFormatting.GREEN + species.getLocalizedName() : I18n.format("tooltip.woodland_staff.tree_not_set")));
-        tooltip.add("JoCode: " + TextFormatting.GOLD + this.getCode(stack));
-        ci.cancel();
+    // Fix the bugged "Â§a" with proper TextFormatting.GREEN
+    // Line 271: tooltip.add(getTranslationText("tooltip.woodland_staff.tree") + " " + (species != null ? "Â§a" + species.getLocalizedName() : getTranslationText("tooltip.woodland_staff.tree_not_set")));
+    private String localizator_DynamicTrees_Staff_addInformation_fixGreenColor(String original) {
+        return "" + TextFormatting.GREEN;
     }
-    
-    @Shadow(remap = false)
-    public Species getSpecies(ItemStack itemStack) { return null; }
-    
-    @Shadow(remap = false)
-    public String getCode(ItemStack itemStack) { return null; }
+
+    @ModifyConstant(
+            method = "addInformation(Lnet/minecraft/item/ItemStack;Lnet/minecraft/world/World;Ljava/util/List;Lnet/minecraft/client/util/ITooltipFlag;)V",
+            constant = @Constant(stringValue = "JoCode: \u00c2\u00a76"),
+            remap = Production.inProduction
+    )
+    // Fix JoCode's bugged "Â§6" with proper TextFormatting.GOLD
+    // Line 272: tooltip.add("JoCode: Â§6" + this.getCode(stack));
+    private String localizator_DynamicTrees_Staff_addInformation_fixJoCodeColor(String original) {
+        return "JoCode: " + TextFormatting.GOLD;
+    }
 }
